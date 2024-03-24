9cef22c18b0679abe24d5de8654edda197a14b36
david.machio <david.machio@MB-07.local>
2024-03-24 20:21:43 +0100

    que le peten a los titulos

--- components/Inicio/Inicio.js
+++ components/Inicio/Inicio.js
@@ -10,11 +10,8 @@ export const Inicio = () => {
         const div = document.createElement('div')
         div.className = 'contenedorImg'
         div.style.backgroundImage = `url(${reloj.img})`
-        const title = document.createElement('h2');
-        title.className = 'title'
-        title.textContent = reloj.marca
+        
 
-        div.append(title)
     main.append(div)
     }
 
